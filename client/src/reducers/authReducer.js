2944684cfb04bebaaf26f9c38490695f8980231b
Emir Balic <emirbalic@gmail.com>
2018-11-11 20:18:08 +0100

    add login form and auth LS

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,3 +1,7 @@
+import isEmpty from '../validation/is-empty';
+
+import { SET_CURRENT_USER } from '../actions/types';
+
 const initalState = {
   isAuthenicated: false,
   user: {}
@@ -5,6 +9,14 @@ const initalState = {
 
 export default function(state = initalState, action) {
   switch (action.type) {
+    case SET_CURRENT_USER:
+      return {
+        ...state,
+        // because the payload will be full it is going to set isAuthenicated to 'true'
+        isAuthenticated: !isEmpty(action.payload),
+        // check again how it works exactly that the user IS actual payload
+        user: action.payload
+      };
     default:
       return state;
   }
---
+++ client/src/utils/setAuthToken.js
@@ -0,0 +1,16 @@
+// Better axios than fetch because if user is loggedIn I call the func and it will
+// always attach the auth header
+
+import axios from 'axios';
+
+const setAuthToken = token => {
+  if (token) {
+    // Apply to every request
+    axios.defaults.headers.common['Authorization'] = token;
+  } else {
+    // Delete auth header
+    delete axios.defaults.headers.common['Authorization'];
+  }
+};
+
+export default setAuthToken;
